f03316cdff11df1bfba5c211818e33a21ddc8676
AJlekCandr_proff <svznikov@yandex.ru>
2024-09-09 21:04:47 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,9 @@
-# Импорт необходимых модулей.
 from aiogram import Router
 
 from .handlers import router as main_handlers_router
 
 
-# Инициализация роутера.
 router = Router(name=__name__)
 
 
-# Подключение к главному роутеру других роутеров.
 router.include_router(main_handlers_router)
